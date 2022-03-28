ec6c4be55d6bd30e8670a58444b8ee3d66106571
Mohammad Khalaf <mohammadkhalaf@live.com>
2022-03-28 15:16:41 +0200

    pages

--- breakie/src/App.tsx
+++ breakie/src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ breakie/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from '../componets/Form';
+
+const Home = () => {
+  return (
+    <>
+      <Form />
+    </>
+  );
+};
+
+export default Home;
