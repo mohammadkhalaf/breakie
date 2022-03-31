61ad6d32d018eb5d9b3c3f3eae4e1880a873f539
Abeer-Eva <54839100+Abeer-Eva@users.noreply.github.com>
2022-03-31 18:55:16 +0200

    #17#10

---
+++ breakie/src/componets/Choosebutton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './css/choose.module.css'
+
+
+const Choosebutton = () => {
+  return (
+    <button  className={classes.choose}>Choose breakie</button>
+  )
+}
+
+export default Choosebutton
--- breakie/src/componets/Filter.tsx
+++ breakie/src/componets/Filter.tsx
@@ -5,12 +5,27 @@ const Filter = () => {
     <section className='filter-box'>
       <article className="flex-item" >
         <h3 className='filterTitle'>fysik</h3>
-
+        <ul className='filterlist'>
+        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />dansa </label><span>1 minuter</span></li>
+        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />dansa </label><span>30 seconder</span></li>
+          <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />dansa </label><span>3 minuter</span></li>
+        </ul>
         </article>
       <article className='flex-item'>
-        <h3 className='filterTitle'>mental</h3></article>
+        <h3 className='filterTitle'>mental</h3>
+        <ul className='filterlist'>
+        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />meditation </label><span>3 minuter</span></li>
+        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />meditation </label> <span>1 minuter</span></li>
+        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />meditation   </label><span>4 minuter</span>
+      </li>
+        </ul></article>
       <article className='flex-item'>
-        <h3 className='filterTitle'>social</h3></article>
+        <h3 className='filterTitle'>social</h3>
+        <ul className='filterlist'>
+        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />spela </label><span>2 minuter</span></li>
+        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />spela </label><span>2 minuter</span></li>
+        
+        </ul></article>
     </section>
   )
 }
