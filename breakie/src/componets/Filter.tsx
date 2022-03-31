56ff56f1aefa1f7b81140a03da1967e0557e2a63
Abeer-Eva <54839100+Abeer-Eva@users.noreply.github.com>
2022-03-31 19:28:05 +0200

    css filterbox

--- breakie/src/componets/Filter.tsx
+++ breakie/src/componets/Filter.tsx
@@ -6,24 +6,24 @@ const Filter = () => {
       <article className="flex-item" >
         <h3 className='filterTitle'>fysik</h3>
         <ul className='filterlist'>
-        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />dansa </label><span>1 minuter</span></li>
-        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />dansa </label><span>30 seconder</span></li>
-          <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />dansa </label><span>3 minuter</span></li>
+        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />dansa <span>1 minuter</span> </label></li>
+        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />dansa <span>30 seconder</span></label></li>
+          <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />dansa <span>3 minuter</span> </label></li>
         </ul>
         </article>
       <article className='flex-item'>
         <h3 className='filterTitle'>mental</h3>
         <ul className='filterlist'>
-        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />meditation </label><span>3 minuter</span></li>
-        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />meditation </label> <span>1 minuter</span></li>
-        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />meditation   </label><span>4 minuter</span>
+        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />meditation<span>3 minuter</span> </label></li>
+        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />meditation<span>1 minuter</span> </label> </li>
+        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />meditation  <span>4 minuter</span>  </label>
       </li>
         </ul></article>
       <article className='flex-item'>
         <h3 className='filterTitle'>social</h3>
         <ul className='filterlist'>
-        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />spela </label><span>2 minuter</span></li>
-        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />spela </label><span>2 minuter</span></li>
+        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />spela <span>2 minuter</span> </label></li>
+        <li className="list__item">  <label className='label--checkbox'><input type="checkbox" />spela  <span>2 minuter</span></label></li>
         
         </ul></article>
     </section>
