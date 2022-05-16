187152fa3429eb43638dcfbad2c921e8482cdd51
Abeer-Eva <54839100+Abeer-Eva@users.noreply.github.com>
2022-05-16 15:41:43 +0200

    .

--- breakie/src/components/filter/Filter.tsx
+++ breakie/src/components/filter/Filter.tsx
@@ -6,7 +6,8 @@ import { useNavigate } from 'react-router-dom';
 import ListItem from '../ListItem/ListItem';
 import Overlay from '../overlay/Overlay';
 import search from '../../assets/search.svg';
-import close from '../../assets/close.png';
+import close from '../../assets/close.png'
+
 
 const Filter = () => {
   const [searchField, setSearchField] = useState('');
@@ -14,75 +15,65 @@ const Filter = () => {
     useCollection('Breakies');
   const [choseList, setChoseList] = useState(new Array());
   const { chooseData, show } = useContext(AppContext);
-  const [active, setActive] = useState(false);
 
-  const [localitems, setLocalItems] = useState(Object);
+const [localitems, setLocalItems]=useState(Object)
+
 
   const navigate = useNavigate();
 
-  //hämta list from localStorge with checked item
-  const storedItems = (items: any) => {
-    console.log('clicked');
-
-    const arr: any = items.item.choseList;
-
-    arr.map((item: any) => {
-      if (item.type === 'fysisk') {
-        let f: any = [...fysisk];
-        let idx: number = f.findIndex((i: any) => i.id === item.id);
-        f[idx].isChecked = !f[idx].isChecked;
-
-        setFysisk(f);
-      } else if (item.type === 'mental') {
-        let m: any = [...mental];
-        let idx: number = m.findIndex((i: any) => i.id === item.id);
-        m[idx].isChecked = !m[idx].isChecked;
-        setMental(m);
-      } else {
-        let s: any = [...social];
-        let idx: number = s.findIndex((i: any) => i.id === item.id);
-        s[idx].isChecked = !s[idx].isChecked;
-        setSocial(s);
-      }
-    });
-
+//hämta list from localStorge with checked item
+  const storedItems=(items:any) =>{
+    
     setLocalItems(items.item);
-    setChoseList([...items.item.choseList]);
-  };
+      const arr:any=items.item.choseList;  
+  
+       console.log(items.item.choseList)
+  setChoseList(arr)
+  
+  arr.map((item:any)=>{
+    if(item.type==="fysisk"){
+      let f: any = [...fysisk];
+      let idx: number = f.findIndex((i: any) => i.id === item.id);
+      f[idx].isChecked = !f[idx].isChecked;
+      console.log( f[idx].isChecked)
+      setFysisk(f);
+    
+    }
+  else  if (item.type === 'mental') {
+      let m: any = [...mental];
+      let idx: number = m.findIndex((i: any) => i.id === item.id);
+      m[idx].isChecked = !m[idx].isChecked;
+      setMental(m);
+    
+  } 
+  else {
+    let s: any = [...social];
+    let idx: number = s.findIndex((i: any) => i.id === item.id);
+    s[idx].isChecked = !s[idx].isChecked;
+    setSocial(s);
+  } })
+ 
+  }
+console.log(localitems);
 
-  const closeStoredName = () => {
-    setChoseList([]);
-    let s = [...social];
 
-    setSocial(
-      s.map((item: any) => {
-        return { ...item, isChecked: false };
-      })
-    );
-    let f = [...fysisk];
-
-    setFysisk(
-      f.map((item: any) => {
-        return { ...item, isChecked: false };
-      })
-    );
-
-    let m = [...mental];
-
-    setMental(
-      m.map((item: any) => {
-        return { ...item, isChecked: false };
-      })
-    );
-
-    setLocalItems({});
-  };
+const closeStoredName=()=>{
+  setLocalItems({});
+  setChoseList([])
+  console.log([...choseList] )
+  // let f: any = [...fysisk,];
+  // let m: any = [...mental];
+  // let s: any = [...social];
+  // setFysisk(f);
+  // setMental(m);
+  // setSocial(s);
+  
+}
 
   const RandomEl = () => {
     chooseData(choseList);
     navigate('/breakie');
   };
-  console.log('clicked 2');
 
   const saveData = (item: any) => {
     //save data in state for slump breakir from manuall
@@ -107,9 +98,6 @@ const Filter = () => {
       setSocial(s);
     }
   };
-
-  console.log(choseList);
-
   const removeItem = (item: any) => {
     //uppdate data with isChecked  in usecollection for localstorge then
     if (item.type === 'mental') {
@@ -131,28 +119,16 @@ const Filter = () => {
 
     setChoseList(choseList.filter((x: any) => x.id !== item.id));
   };
-
-  const activeBar = () => {
-    setActive(!active);
-  };
-
+  console.log(choseList);
   return (
     <>
       {show && <Overlay choseList={choseList} storedItems={storedItems} />}
       <div className={classes.wrapper}>
-        <div
-          className={
-            active
-              ? `${classes.searchBar} ${classes.active}`
-              : `${classes.searchBar}`
-          }
-        >
-          <img
-            src={search}
-            alt=''
-            className={classes.searchicon}
-            onClick={activeBar}
-          />
+        <div className={classes.searchBar}  >
+          {/* <FaSearch className={classes.searchicon} /> */}
+          <img src={search} alt='search' className={classes.searchicon}  />
+          {/* `classes${isActive?".search":".search__close"}` */}
+          <div>
           <input
             className={classes.search}
             placeholder=''
@@ -160,14 +136,15 @@ const Filter = () => {
             value={searchField}
             onChange={(e) => setSearchField(e.target.value)}
           />
+          </div>
+         
         </div>
-
-        {localitems.name ? (
-          <span className={classes.storedLocal}>
-            <img src={close} alt='close' onClick={() => closeStoredName()} />
+         
+        {localitems.name?<span className={classes.storedLocal}>
+            <img src={close} alt="close"  onClick={()=>closeStoredName()}/>
             <span>{localitems.name}</span>
-          </span>
-        ) : null}
+
+        </span>:null}
         <section className={classes.filter_box}>
           <article className={classes.flex_item}>
             <h3 className={classes.filterTitle}>fysisk</h3>
@@ -225,12 +202,8 @@ const Filter = () => {
           </article>
         </section>
 
-        <button
-          className={classes.choose}
-          onClick={RandomEl}
-          disabled={choseList.length < 1}
-        >
-          {choseList.length > 0 ? 'slumpa fram en breakie' : 'välj breakie'}
+        <button className={classes.choose} onClick={RandomEl} disabled={choseList.length<1}>
+        {choseList.length>0?"slumpa from en breakie":"välj breakie" }
         </button>
       </div>
     </>
