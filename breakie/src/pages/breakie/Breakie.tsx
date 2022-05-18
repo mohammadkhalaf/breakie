8710cac8d7e01e734e3e35f563ed439dddb62beb
mohammad <mohammadkhalaf@live.com>
2022-05-18 12:03:51 +0200

    #44

--- breakie/src/pages/breakie/Breakie.tsx
+++ breakie/src/pages/breakie/Breakie.tsx
@@ -1,14 +1,19 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { AppContext } from '../../context/activityContext';
 import { DocumentData } from 'firebase/firestore';
 import classes from './breakie.module.css';
 import pic from '../../assets/pic.svg';
+import { useNavigate } from 'react-router-dom';
 
 const Breakie = () => {
   const { activities } = useContext(AppContext);
   const [data, setData] = useState<DocumentData[] | null>(Array);
   const [random, setRandom] = useState(Object);
   const [loading, setLoading] = useState(false);
+
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const navigate = useNavigate();
 
   const getRandom = async () => {
     //Random Breakie
@@ -17,16 +22,42 @@ const Breakie = () => {
       activities[Math.floor(Math.random() * activities.length)];
     setData(activities);
     setRandom(randomElement);
+
+    setMinutes(randomElement?.time);
+    setSeconds(0);
+
     setLoading(false);
   };
 
+  const updateRemainingTime = useCallback((s: any, m: any) => {
+    setSeconds(s);
+    setMinutes(m);
+  }, []);
+
+  useEffect(() => {
+    let s = seconds;
+    let m = minutes;
+    const int = setInterval(() => {
+      s--;
+      if (s < 0) {
+        m--;
+        s = 59;
+        if (m < 0) {
+          navigate('/');
+        }
+      }
+      updateRemainingTime(s, m);
+    }, 1000);
+    return () => {
+      clearInterval(int);
+    };
+  }, [random]);
   useEffect(() => {
     getRandom();
   }, [activities]);
 
   let randomUrl;
   if (random && random.URL) {
-    console.log(`${random.URL}`);
     //Ändra URL till new URl som replace embed istället watch?= då funkar youtube video
     const newURL = random.URL.replace('watch?v=', 'embed/');
 
@@ -48,11 +79,13 @@ const Breakie = () => {
               <h1>{random.name}</h1>
               <div className={classes.info}>
                 <div className={classes.type}>
-                  <img src={pic} alt='breakie' />
                   <span>{random.type}</span>
                 </div>
                 <div>
-                  <span className={classes.time}>{random.time}</span>
+                  <span className={classes.time}>
+                    0{minutes}:{seconds < 10 ? 0 : null}
+                    {seconds}
+                  </span>
                   <span>minuter</span>
                 </div>
               </div>
